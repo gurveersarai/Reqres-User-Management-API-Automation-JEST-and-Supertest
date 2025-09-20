beda3da772bcaf874f6008170207858717d00b60
gurveersarai <saraiguv@googlemail.com>
2025-09-20 11:09:42 +0100

    Added Jest Stare Report

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,15 @@
-// jest.config.js
 export default {
   testEnvironment: "node",
   verbose: true,
   testTimeout: 30000,
+  reporters: [
+    "default",
+    [
+      "jest-stare",
+      {
+        resultDir: "reports/jest-stare",
+        reportTitle: "Reqres API Test Report",
+      },
+    ],
+  ],
 };
